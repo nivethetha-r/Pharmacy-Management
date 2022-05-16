76e318880c999cb7aec571f319b0ed3378ab451a
Nivethetha <nivethethaa@gmail.com>
2022-05-16 10:22:04 +0530

    vd

---
+++ HelloWorld!/src/mrCooper/B2.java
@@ -0,0 +1,15 @@
+package mrCooper;
+
+public class B2 extends B1
+{
+		public B2(int a, int b)
+		{
+			super(a,b);
+		}
+		protected int c = 10;
+		protected void add()
+		{
+			sum(10,20);
+		}
+
+}
